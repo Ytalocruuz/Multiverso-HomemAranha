e8a063ae40a94fcf5088e64447dae20ed4ccb66a
Ytalo Cruz <ytalocrz@gmail.com>
2023-03-08 20:13:11 -0400

    Estrutura e Estilizações

---
+++ assets/scripts/card-hover-animation.js
@@ -0,0 +1,7 @@
+// function addEventiListenersToCards(){
+//     const cardElements = document.getElementsByClassName('s-card');
+// }
+
+// document.addEventListener("DOMContentLoaded", addEventiListenersToCards, false);
+
+// // Funcão apenas funcinarar, assim que carregar a página inteira. False: Pq é opcional 
