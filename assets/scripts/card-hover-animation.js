a80cc2d5465a07701c2c0693bdaf470df3bc60af
Ytalo Cruz <ytalocrz@gmail.com>
2023-03-09 19:51:01 -0400

    Parte lógica  com JavaScript, e um pouco de estilização

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -1,7 +1,22 @@
-// function addEventiListenersToCards(){
-//     const cardElements = document.getElementsByClassName('s-card');
-// }
+function handleMouseEnter(){
+    this.classList.add('s-card--Hovered');
+    document.body.id =`${this.id}-hovered`;
+}
+function handleMouseLeave(){
+    this.classList.remove('s-card--Hovered')
+    document.body.id = '';
+    // Para remover
+}
 
-// document.addEventListener("DOMContentLoaded", addEventiListenersToCards, false);
+function addEventiListenersToCards(){
+    const cardElements = document.getElementsByClassName('s-card');
+    for(let index = 0; index < cardElements.length; index++){
+        const card = cardElements[index];
+        card.addEventListener('mouseenter', handleMouseEnter);
+        card.addEventListener('mouseleave', handleMouseLeave);
+    }
+}
 
-// // Funcão apenas funcinarar, assim que carregar a página inteira. False: Pq é opcional 
+document.addEventListener("DOMContentLoaded", addEventiListenersToCards, false);
+
+// Funcão apenas funcinarar, assim que carregar a página inteira. False: informando que não quero passar nenhuma paraametro, Pq é opcional 
